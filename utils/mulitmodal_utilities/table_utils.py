0d67babdfae42b44b1d1259ca2c6e1f25ce85d61
codym_snova <cody.macdonald@sambanovasystems.com>
2024-09-04 17:46:39 -0500

    added augmentations

--- utils/mulitmodal_utilities/table_utils.py
+++ utils/mulitmodal_utilities/table_utils.py
@@ -5,6 +5,7 @@ import logging
 import numpy as np
 from pathlib import Path
 from PIL import Image
+import albumentations as A
 from pdf2image import convert_from_path
 import random
 import subprocess
@@ -54,7 +55,6 @@ font_types = [
     "mathpazo",
     "newcent"
 ]
-
 
 
 class TableTools:
@@ -140,42 +140,96 @@ class TableTools:
 
         return text
     
+    def _return_latex_text(self, text: str):
+        
+        latex_text = ""
+
+        for line in text.splitlines():
+            line = line.replace("\t", " & ")
+            line += " \\\\\n"
+            latex_text += line
+
+        return latex_text
+    
     def _return_colors(self) -> List[str]:
 
         colors = ["\\definecolor{mycolor}{RGB}{150,210,255}\n",
                   "\\definecolor{mycolor}{RGB}{210,210,210}\n",
                   "\\definecolor{mycolor}{RGB}{175,250,180}\n",
-                  "\\definecolor{mycolor}{RGB}{250,250,180}\n",
-                  "\\definecolor{mycolor}{RGB}{176,247,171}\n"]
+                  "\\definecolor{mycolor}{RGB}{250,250,180}\n",]
         
         return colors
+    
+    def _randomize_colors(self, text: str):
 
-    def convert_tsv_to_latex(self, 
-                             text: str,
-                             randomize_colors: bool = True,
-                             ) -> str:
-
-        text = self.replace_special_to_latex(text)
+        r = np.random.uniform(0, 1)
 
         latex_text = ""
-        
-        r = np.random.uniform(0, 1)
 
         for i ,line in enumerate(text.splitlines()):
 
             line = line.replace("\t", " & ")
 
-            if randomize_colors:
-                if 0.5 <= r < 0.75:
-                    if i%2 == 0:
-                        line = "\\rowcolor{mycolor}\n" + line
-                if 0.76 <= r < 1.0:
-                    if i%2 == 1:
-                        line = "\\rowcolor{mycolor}\n" + line
+            if 0.5 <= r < 0.75:
+                if i%2 == 0:
+                    line = "\\rowcolor{mycolor}\n" + line
+            if 0.76 <= r < 1.0:
+                if i%2 == 1:
+                    line = "\\rowcolor{mycolor}\n" + line
 
             line += " \\\\\n"
-
             latex_text += line
+
+        return latex_text
+    
+    def _randomly_add_vert_lines(self, table_string, prob):
+        # Split the string into lines
+        lines = table_string.split('\n')
+
+        # Find the line with the column definitions
+        for i, line in enumerate(lines):
+            if '{' in line and '}' in line:
+                column_definitions = line
+                break
+
+        # Split the column definitions into individual columns
+        columns = column_definitions.strip().strip('{}').split()
+
+        # Randomly add pipes between the columns
+        new_columns = []
+        for column in columns:
+            if column in ['l', 'c', 'r']:
+                if random.random() < prob:  # % chance of adding a pipe
+                    new_columns.append(column + ' |')
+                else:
+                    new_columns.append(column)
+            else:
+                new_columns.append(column)
+
+        # Join the new columns back into a string
+        new_column_definitions = '{ ' + ' '.join(new_columns) + ' }'
+
+        # Replace the old column definitions with the new ones
+        lines[i] = new_column_definitions
+
+        # Join the lines back into a single string
+        new_table_string = '\n'.join(lines)
+
+        return new_table_string
+
+    def convert_tsv_to_latex(self, 
+                             text: str,
+                             randomize_colors: bool = True,
+                             randomize_vertical_lines: bool = True,
+                             vertical_line_prob: int = 0.5
+                             ) -> str:
+
+        text = self.replace_special_to_latex(text)
+
+        if randomize_colors:
+            latex_text = self._randomize_colors(text)
+        else:
+            latex_text = self._return_latex_text(text)
 
         header1 = "\\usepackage{colortbl}\n\\usepackage{xcolor}\n" 
         colors = self._return_colors() #Simple return method to declutter
@@ -188,7 +242,12 @@ class TableTools:
         # column_style = "\\begin{tabular}{| l c c c c c c c |}\n\\hline\n"
         # column_style = "\\begin{tabular}{| l c c c |}\n\\hline\n"
         # column_style = "\\begin{tabular}{| l c c |}\n\\hline\n"
+
         column_style = "\\begin{tabular}{| l c c r c c r |}\n\\hline\n"
+        
+        if randomize_vertical_lines:
+            column_style = self._randomly_add_vert_lines(column_style, vertical_line_prob)
+
         footer = "\\hline\n\\end{tabular}\n\\end{table}\n\\end{document}"
 
         formatted_latex_text = header + column_style + latex_text + footer
@@ -240,8 +299,8 @@ class TableTools:
         with open(tex_filepath, 'w') as f:
             f.write(latex_code)
 
-    def _crop_table(self, image_path: str):
-        # Load the image in grayscale
+    def _crop_synth_table(self, image_path: str):
+        # Load the image natrually and in grayscale
         image = cv2.imread(image_path)
         gray_image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         
@@ -269,7 +328,6 @@ class TableTools:
                 cv2.imwrite(image_path, cropped_table)
                 keeptrying = False
             except Exception as e:
-                print("---EXCEPTION---")
                 print(e)
                 time.sleep(0.5)
                 counter += 1
@@ -280,7 +338,49 @@ class TableTools:
 
         # return cropped_table, cropped_table.shape
     
-    def _generate_images(self, folder_name, data) -> None:
+    def _image_augmentations(self, 
+                             augmentations: List[str] = [
+                                 "RandomBrightnessContrast",
+                                 "Blur",
+                                 "Downscale",
+                                 "GaussNoise",
+                                 "ISONoise",
+                                 "ImageCompression",
+                                 "RandomGamma",
+                                 "ToGray"
+                             ]
+                             ):
+        
+        transformations_map = {
+            "RandomBrightnessContrast": A.RandomBrightnessContrast(p=0.5),
+            "Blur": A.Blur(p=0.5, blur_limit=(3,5)),
+            "Downscale": A.Downscale(p=0.5, scale_min=0.55, scale_max=0.85),
+            "GaussNoise": A.GaussNoise(p=0.5, var_limit=(50.0, 250.0)),
+            "ISONoise": A.ISONoise(p=0.2, color_shift=(0.1, 0.5), intensity=(0.1, 0.5)),
+            "ImageCompression": A.ImageCompression(p=0.5, quality_lower=50, quality_upper=90),
+            "RandomGamma": A.RandomGamma(p=0.5, gamma_limit=(50, 150)),
+            "ToGray": A.ToGray(p=0.5), 
+        }
+
+        assert all(item in transformations_map.keys() for item in augmentations), \
+            f"Selected augmentations must be in: {transformations_map.keys()}."
+        
+        pipeline = []
+
+        for name in augmentations:
+            transformation = transformations_map.get(name)
+            if transformation:
+                pipeline.append(transformation)
+        
+        transform = A.Compose(pipeline)
+
+        return transform
+    
+    def _generate_images(self, 
+                         folder_name, 
+                         data,
+                         randomize_dpi: bool = True,
+                         use_augmentations: bool = True) -> None:
     
         # for i, d in enumerate(data):
             # table_latex = d["latex"]
@@ -307,13 +407,23 @@ class TableTools:
         subprocess.run(['mv', 'table.pdf', pdf_filepath])
 
         #TODO: Randomize DPI
-        images = convert_from_path(pdf_filepath, dpi=250)
+        if randomize_dpi:
+            images = convert_from_path(pdf_filepath, dpi=np.random.choice([75, 120, 250, 300]))
+        else:
+            images = convert_from_path(pdf_filepath, dpi=250)
         image = images[-1] # Sometimes more than one page is generated, so we take the last one
         image_size = image.size  # This returns a tuple (width, height)
         print(image_size)
+
+        if use_augmentations:
+            transform = self._image_augmentations()
+            image = transform(image=np.array(image))['image']
+            image = image.astype(np.uint8)
+            image = Image.fromarray(image)
+
         image.save(img_filepath, 'JPEG')
         
-        self._crop_table(str(img_filepath))
+        self._crop_synth_table(str(img_filepath))
         subprocess.run(['rm', tex_filepath])
         subprocess.run(['rm', pdf_filepath])
     
